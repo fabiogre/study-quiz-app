13336607af5602d90c3f99e896fb711a43a83aa1
Fabio Greco <fabiogreco@web.de>
2026-03-24 11:27:58 +0100

    chore: configure supabase cloud profile

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 ﻿window.STUDY_QUIZ_CLOUD = {
-  enabled: false,
-  supabaseUrl: "",
-  anonKey: "",
+  enabled: true,
+  supabaseUrl: "https://umxhgkskmtemdzgnilmj.supabase.co",
+  anonKey: "sb_publishable_6zeS4O7SOk8Vc7gRAexGSA_GeQOYQtz",
   appName: "Study Quiz",
 };
